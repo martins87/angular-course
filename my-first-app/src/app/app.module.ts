72ba735ffc5d3220d1f183dd70d24349237f0816
Daniel Martins <martinsda@gmail.com>
2019-09-18 23:22:36 -0300

    Add first component

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+// import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
+// NgModule decorator
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    // HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+// decorator: a type that allows you to enhance your class
+@Component({
+    // the selector must be unique
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
