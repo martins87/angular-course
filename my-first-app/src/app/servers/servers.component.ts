36ac7d542ddd5a621e749856ac4c41430efbe74b
Daniel Martins <martinsda@gmail.com>
2019-09-21 00:09:50 -0300

    Edit first event binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowNewServer: boolean = false;
-  serverCreationStatus = 'No server was created';
+  serverCreationStatus: string = 'No server was created';
+  serverName: string = '';
 
   constructor() {
     setTimeout(() => {
@@ -21,6 +22,10 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
