82eded907749833c6821886072a87318285f6c01
Daniel Martins <martinsda@gmail.com>
2019-09-21 00:14:27 -0300

    Edit first event binding 2

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -9,7 +9,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No server was created';
-  serverName: string = '';
+  serverName: string = 'Test Server';
 
   constructor() {
     setTimeout(() => {
