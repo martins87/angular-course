b1cc4adb3e8bb004281e1a5392fe4d824298ab1e
Daniel Martins <martinsda@gmail.com>
2019-09-20 23:45:00 -0300

    Add first event binding

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 // NgModule decorator
 @NgModule({
   declarations: [
     AppComponent,
     WarningAlertComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+
+  getServerId() {
+    return this.serverId;
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  ngOnInit() {}
+
+}
