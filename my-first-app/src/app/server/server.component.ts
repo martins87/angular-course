33bd0e889ec3964944f792bcc2ba5726c01cfe3f
Daniel Martins <martinsda@gmail.com>
2019-09-23 10:30:07 -0300

    Edit 1st directive: ngIf

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -3,21 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  // styleUrls: ['./server.component.css']
+  styles: [`
+    .online {
+      color: white;
+    }
+  `]
 })
 export class ServerComponent implements OnInit {
 
   serverId: number = 10;
   serverStatus: string = 'offline';
 
-  getServerId() {
-    return this.serverId;
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
   }
+
+  ngOnInit() {}
 
   getServerStatus() {
     return this.serverStatus;
   }
 
-  ngOnInit() {}
+  getColor() {
+    return this.serverStatus === 'online'? 'green' : 'red';
+    }
 
 }
