2367da2f3a7a51762a0a0856d915e7a1222a64e4
Daniel Martins <martinsda@gmail.com>
2019-10-02 10:34:32 -0300

    Edit shopping-edit

--- course-project/src/app/shared/ingredient.model.ts
+++ course-project/src/app/shared/ingredient.model.ts
@@ -1,9 +1,3 @@
 export class Ingredient {
-    public name: String;
-    public amount: number;
-
-    constructor(name: String, amount: number) {
-        this.name = name;
-        this.amount = amount;
-    }
+    constructor(public name: String, public amount: number) {}
 }
